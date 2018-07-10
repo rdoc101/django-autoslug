f317f47f14b1d1e5097215a6334394d16f2cb9c7
Richard <richardflett@gmail.com>
2018-07-10 12:18:08 +1200

    changing for django 2 compat

--- autoslug/settings.py
+++ autoslug/settings.py
@@ -60,7 +60,7 @@ Django settings that affect django-autoslug:
 from django.conf import settings
 try:
     from django.core.urlresolvers import get_callable
-except ImportError:
+except ModuleNotFoundError:
     from django.urls import get_callable
 
 # use custom slugifying function if any
